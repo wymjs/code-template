2d0e02f482ebcc81973b14c6b349959681c654c7
twjw <aaaawjwa@gmail.com>
2024-08-10 15:17:18 +0800

    feat: add auto-increment

---
+++ auto-increment/main.ts
@@ -0,0 +1,56 @@
+type AutoIncrementInstance<T extends string | number> = {
+  value: T
+  next(): T
+}
+
+const a_z = 'abcdefghijklmnopqrstuvwxyz'
+const A_Z = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const A_z = a_z + A_Z
+
+function autoIncrement<T extends string | number>(n: T): AutoIncrementInstance<T> {
+  if (typeof n === 'number') {
+    return {
+      value: n,
+      next() {
+        ;(this.value as number)++
+        return this.value
+      },
+    }
+  }
+
+  let idxes = [] as number[]
+  let idx = 0
+
+  return {
+    value: n[0] as T,
+    next() {
+      if (idxes.length === 0) {
+        idxes.push(0)
+      } else {
+        if (idxes[idx] + 1 === n.length) {
+          if (idxes.length > 1) {
+            for (let i = idx - 1; i >= 0; i--) {
+              if (idxes[i] + 1 < n.length) {
+                for (let j = i + 1; j < idxes.length; j++) {
+                  idxes[j] = 0
+                }
+                idxes[i]++
+                break
+              } else if (i === 0) {
+                idxes = Array((idx = idxes.length) + 1).fill(0)
+              }
+            }
+          } else {
+            idxes = Array((idx = idxes.length) + 1).fill(0)
+          }
+        } else {
+          idxes[idx]++
+        }
+      }
+
+      return idxes.reduce<string>((p, i) => p + n[i], '') as T
+    },
+  }
+}
+
+export { type AutoIncrementInstance, a_z, A_Z, A_z, autoIncrement }
---
+++ auto-increment/readme.md
@@ -0,0 +1,25 @@
+自增值
+===
+
+```typescript
+// 數字版(就數字自增而已)
+const numId = autoIncrement(0)
+console.log(numId.value) // 0
+console.log(numId.next()) // 1
+console.log(numId.value) // 1
+console.log(numId.next()) // 2
+console.log(numId.next()) // 3
+
+
+// 字串版，傳入的是字串，運行方式看下面應該就懂了
+const enId = autoIncrement('ab')
+console.log(enId.value) // a
+console.log(enId.next()) // b
+console.log(enId.next()) // aa
+console.log(enId.next()) // ab
+console.log(enId.next()) // ba
+console.log(enId.next()) // bb
+console.log(enId.next()) // aaa
+console.log(enId.next()) // aab
+// ...
+```
