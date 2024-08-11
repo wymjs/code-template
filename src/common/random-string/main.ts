3f97403250a6671d106000fcf7a3ccc3478877f1
twjw <aaaawjwa@gmail.com>
2024-08-11 19:21:53 +0800

    feat: add random string

---
+++ src/common/random-string/main.ts
@@ -0,0 +1,14 @@
+// code 可以改成自己要的
+const code = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+function randomString(length = 36) {
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += code.charAt(Math.floor(Math.random() * code.length));
+  }
+
+  return result;
+}
+
+export { randomString };
---
+++ src/common/random-string/readme.md
@@ -0,0 +1,9 @@
+random-string
+===
+
+> 可調長度的隨機字串
+
+```typescript
+// 一參為亂碼長度
+const id = randomString(12) // 'nkj12jkfhkaj'
+```
